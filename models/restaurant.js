d4220233458ac3469d29f48c4986dbb40f32b4fb
tyleralves <tyler11alves@gmail.com>
2017-05-05 21:20:10 -0700

    Add photo field to Restaurants table

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,7 +3,8 @@ module.exports = function(sequelize, DataTypes) {
   var Restaurant = sequelize.define('Restaurant', {
     name: {type: DataTypes.STRING, allowNull: false},
     phone: DataTypes.INTEGER,
-    address: DataTypes.STRING
+    address: DataTypes.STRING,
+    photo: DataTypes.STRING
   }, {
     classMethods: {
       associate: function(models) {
